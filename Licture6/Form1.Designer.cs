14c37ea4c9f889cfcadce3a68bfd0032695b7df8
SulimanDahan <90277725+SulimanDahan@users.noreply.github.com>
2021-10-24 10:05:12 -0700

    Add files via upload

---
+++ Licture6/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Licture6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.OpenB = new System.Windows.Forms.ToolStripButton();
+            this.CopyB = new System.Windows.Forms.ToolStripButton();
+            this.PasteB = new System.Windows.Forms.ToolStripButton();
+            this.FrameB = new System.Windows.Forms.ToolStripButton();
+            this.ScreenShotB = new System.Windows.Forms.ToolStripButton();
+            this.Photo1 = new System.Windows.Forms.PictureBox();
+            this.Photo2 = new System.Windows.Forms.PictureBox();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Photo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Photo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenB,
+            this.CopyB,
+            this.PasteB,
+            this.FrameB,
+            this.ScreenShotB});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // OpenB
+            // 
+            this.OpenB.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.OpenB.Image = ((System.Drawing.Image)(resources.GetObject("OpenB.Image")));
+            this.OpenB.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.OpenB.Name = "OpenB";
+            this.OpenB.Size = new System.Drawing.Size(40, 22);
+            this.OpenB.Text = "Open";
+            this.OpenB.Click += new System.EventHandler(this.OpenB_Click);
+            // 
+            // CopyB
+            // 
+            this.CopyB.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.CopyB.Enabled = false;
+            this.CopyB.Image = ((System.Drawing.Image)(resources.GetObject("CopyB.Image")));
+            this.CopyB.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CopyB.Name = "CopyB";
+            this.CopyB.Size = new System.Drawing.Size(39, 22);
+            this.CopyB.Text = "Copy";
+            this.CopyB.Click += new System.EventHandler(this.CopyB_Click);
+            // 
+            // PasteB
+            // 
+            this.PasteB.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.PasteB.Enabled = false;
+            this.PasteB.Image = ((System.Drawing.Image)(resources.GetObject("PasteB.Image")));
+            this.PasteB.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.PasteB.Name = "PasteB";
+            this.PasteB.Size = new System.Drawing.Size(39, 22);
+            this.PasteB.Text = "Paste";
+            this.PasteB.Click += new System.EventHandler(this.PasteB_Click);
+            // 
+            // FrameB
+            // 
+            this.FrameB.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.FrameB.Enabled = false;
+            this.FrameB.Image = ((System.Drawing.Image)(resources.GetObject("FrameB.Image")));
+            this.FrameB.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.FrameB.Name = "FrameB";
+            this.FrameB.Size = new System.Drawing.Size(44, 22);
+            this.FrameB.Text = "Frame";
+            this.FrameB.Click += new System.EventHandler(this.FrameB_Click);
+            // 
+            // ScreenShotB
+            // 
+            this.ScreenShotB.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ScreenShotB.Image = ((System.Drawing.Image)(resources.GetObject("ScreenShotB.Image")));
+            this.ScreenShotB.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ScreenShotB.Name = "ScreenShotB";
+            this.ScreenShotB.Size = new System.Drawing.Size(69, 22);
+            this.ScreenShotB.Text = "Screenshot";
+            this.ScreenShotB.Click += new System.EventHandler(this.ScreenShotB_Click);
+            // 
+            // Photo1
+            // 
+            this.Photo1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Photo1.Location = new System.Drawing.Point(0, 25);
+            this.Photo1.Name = "Photo1";
+            this.Photo1.Size = new System.Drawing.Size(470, 425);
+            this.Photo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Photo1.TabIndex = 1;
+            this.Photo1.TabStop = false;
+            this.Photo1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Photo1_DragDrop);
+            this.Photo1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Photo1_DragEnter);
+            this.Photo1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Photo2_MouseDown);
+            // 
+            // Photo2
+            // 
+            this.Photo2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Photo2.Location = new System.Drawing.Point(470, 25);
+            this.Photo2.Name = "Photo2";
+            this.Photo2.Size = new System.Drawing.Size(330, 425);
+            this.Photo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Photo2.TabIndex = 2;
+            this.Photo2.TabStop = false;
+            this.Photo2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Photo2_DragDrop);
+            this.Photo2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Photo2_DragEnter);
+            // 
+            // OFD
+            // 
+            this.OFD.Filter = "JPG|*.jpg|PNG|*.png";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Photo2);
+            this.Controls.Add(this.Photo1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Photo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Photo2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton OpenB;
+        private System.Windows.Forms.ToolStripButton CopyB;
+        private System.Windows.Forms.ToolStripButton PasteB;
+        private System.Windows.Forms.ToolStripButton FrameB;
+        private System.Windows.Forms.ToolStripButton ScreenShotB;
+        private System.Windows.Forms.PictureBox Photo1;
+        private System.Windows.Forms.PictureBox Photo2;
+        private System.Windows.Forms.OpenFileDialog OFD;
+    }
+}
+
---
+++ Licture6/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Licture6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Photo1.AllowDrop = Photo2.AllowDrop = true;     // A control attribute that allows drag and drop
+        }
+
+        private void OpenB_Click(object sender, EventArgs e)
+        {
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                Photo1.Image = Bitmap.FromFile(OFD.FileName);
+                CopyB.Enabled = FrameB.Enabled = true;
+            }
+        }
+
+        private void CopyB_Click(object sender, EventArgs e)
+        {
+            if(Photo1.Image != null)
+            {
+                Clipboard.SetImage(Photo1.Image);   // Copy image to the clipborad
+                PasteB.Enabled = true;
+            }
+        }
+
+        private void PasteB_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                Photo2.Image = Clipboard.GetImage();    // Get the image from the clipboard
+                PasteB.Enabled = false;
+            }
+        }
+
+        private void Photo1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Photo1_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+
+            if(data != null)
+            {
+                var filename = data as string[];
+                if (filename.Length > 0)
+                {
+                    Photo1.Image = Bitmap.FromFile(filename[0]);
+                    CopyB.Enabled = FrameB.Enabled = (FrameB.Enabled == false) ? true : false;
+                }
+            }
+        }
+
+        private void Photo2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && Photo1.Image != null)
+            {
+                if (DoDragDrop(Photo1.Image, DragDropEffects.Move) == DragDropEffects.Move)
+                    Photo1.Image = null;
+            }
+        }
+
+        private void Photo2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Bitmap)) e.Effect = DragDropEffects.Move;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void Photo2_DragDrop(object sender, DragEventArgs e)
+        {
+            Photo2.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap, true);
+            CopyB.Enabled = PasteB.Enabled = FrameB.Enabled = false;
+        }
+
+        private void FrameB_Click(object sender, EventArgs e)
+        {
+            if (Photo1.Image != null)
+            {
+                Bitmap image = (Bitmap)Photo1.Image;        // Store original iamge
+                Bitmap layer = (Bitmap)Image.FromFile("frame.png");     // Store the frame
+                Bitmap FinalImage = new Bitmap(image.Width, image.Height);  /* Create new empty image having same 
+                                                                               height and width of original image*/
+                using (Graphics graph = Graphics.FromImage(FinalImage))     // Create a graphics object to draw
+                {                                                           // the two images as one 
+                    graph.DrawImage(image, 0, 0, image.Width, image.Height);    // draw the original image in the image
+                    graph.DrawImage(layer, 0, 0, image.Width, image.Height);    // draw the frame in the new image with size of original image
+                }
+                Photo1.Image = FinalImage;  // Insert the new image instead of the original one
+            }
+        }
+
+        private void ScreenShotB_Click(object sender, EventArgs e)
+        {
+            this.Hide();        //Hide the screen of the program
+            System.Threading.Thread.Sleep(1000);
+            SendKeys.Send("{PRTSC}");   // a keyboad shortcut(sendkey) to take a screenshot and save it in the clipboard
+            Photo1.Image = (Image)Clipboard.GetImage();      // Get tht screen shot form clipboard Insert the screenshot in the picturebox
+            this.Show();            // Show program window
+            
+            
+            /*
+            // Another method to take a screenshot
+            // Create a new empty image with size of device screen
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            Photo1.Image = bitmap;
+            this.Show();
+            */
+        }
+    }
+}
